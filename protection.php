59adf9096645af199d259dca4d62e587ef01c244
Ilham Surya Pratama <ilhamsurya26@gmail.com>
2020-10-14 19:03:13 +0700

    fixing protection file

--- protection.php
+++ protection.php
@@ -3,43 +3,40 @@ require_once('koneksi.php');
 require "php-jwt-example/api/vendor/autoload.php";
 use \Firebase\JWT\JWT;
 
-$secret_key = "miftah";
-$jwt = null;
+function isAuthorize() {
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+    header("Access-Control-Allow-Methods: POST");
+    header("Access-Control-Max-Age: 3600");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-$data = json_decode(file_get_contents("php://input"));
+    $secret_key = "miftah";
 
-$authHeader = $_SERVER['HTTP_AUTHORIZATION'];
-$arr = explode(" ", $authHeader);
+    $headers = apache_request_headers();
 
+    if($headers['Authorization']){
 
-/*echo json_encode(array(
-    "message" => "sd" .$arr[1]
-));*/
+        try {
 
-$jwt = $arr[1];
+            $decoded = JWT::decode($headers['Authorization'], $secret_key, array('HS256'));
 
-if($jwt){
+            // Access is granted. Add code of the operation here 
 
-    try {
+            return true;
+            // echo json_encode(array(
+            //     "message" => "Access granted:",
+            //     // "error" => $e->getMessage()
+            // ));
 
-        $decoded = JWT::decode($jwt, $secret_key, array('HS256'));
+        }catch (Exception $e){
 
-        // Access is granted. Add code of the operation here 
+            http_response_code(401);
 
-        echo json_encode(array(
-            "message" => "Access granted:",
-            "error" => $e->getMessage()
-        ));
-
-    }catch (Exception $e){
-
-    http_response_code(401);
-
-    echo json_encode(array(
-        "message" => "Access denied.",
-        "error" => $e->getMessage()
-    ));
-}
-
+            echo json_encode(array(
+                "message" => "Access denied.",
+                "error" => $e->getMessage()
+            ));
+        }
+    }
 }
 ?>
